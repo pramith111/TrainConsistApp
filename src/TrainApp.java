3b81c4c6b00917ea419b654f071d691cfdee7b5d
pramith111 <lpramith111@gmail.com>
2026-03-12 10:18:59 +0530

    UC10: Count Total Seats using reduce

--- src/TrainApp.java
+++ src/TrainApp.java
@@ -1,14 +1,21 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrainApp {
     public static void main(String[] args) {
-        System.out.println("=== UC6: Bogie Capacity Map ===");
-        HashMap<String, Integer> capacityMap = new HashMap<>();
-        capacityMap.put("Sleeper", 72);
-        capacityMap.put("AC Chair", 64);
-        capacityMap.put("First Class", 18);
-        for (var entry : capacityMap.entrySet()) {
-            System.out.println(entry.getKey() + " → " + entry.getValue() + " seats");
-        }
+
+        System.out.println("=== UC10: Total Seating Capacity ===");
+
+        List<Bogie> bogies = new ArrayList<>();
+        bogies.add(new Bogie("Sleeper", 72, "Passenger"));
+        bogies.add(new Bogie("AC Chair", 56, "Passenger"));
+        bogies.add(new Bogie("First Class", 18, "Passenger"));
+
+        // Sum all capacities
+        int total = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
+
+        System.out.println("Total Seating Capacity: " + total + " seats");
     }
 }
