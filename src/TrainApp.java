30b4354c2e2f0ca34747719344916e04a6dcef38
pramith111 <lpramith111@gmail.com>
2026-03-12 10:11:11 +0530

    UC4: Maintain Ordered Bogies using LinkedList

--- src/TrainApp.java
+++ src/TrainApp.java
@@ -1,12 +1,19 @@
-import java.util.HashSet;
+import java.util.LinkedList;
 
 public class TrainApp {
     public static void main(String[] args) {
-        System.out.println("=== UC3: Unique Bogie IDs ===");
-        HashSet<String> bogieIds = new HashSet<>();
-        bogieIds.add("BG101");
-        bogieIds.add("BG102");
-        bogieIds.add("BG101"); // duplicate
-        System.out.println("Bogie IDs: " + bogieIds);
+        System.out.println("=== UC4: Ordered Train Consist ===");
+        LinkedList<String> consist = new LinkedList<>();
+        consist.add("Engine");
+        consist.add("Sleeper");
+        consist.add("AC");
+        consist.add("Cargo");
+        consist.add("Guard");
+        System.out.println("Initial: " + consist);
+        consist.add(2, "Pantry Car");
+        System.out.println("After inserting Pantry Car: " + consist);
+        consist.removeFirst();
+        consist.removeLast();
+        System.out.println("After removing first & last: " + consist);
     }
 }
