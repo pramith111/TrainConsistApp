1f3ed031b48ed2894b1439b1ae2ca50478270993
pramith111 <lpramith111@gmail.com>
2026-03-12 10:06:10 +0530

    Initial project setup

---
+++ src/TrainApp.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainApp {
+    public static void main(String[] args) {
+
+        // UC1: Initialize
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> consist = new ArrayList<>();
+
+        System.out.println("Initial Bogie Count: " + consist.size());
+    }
+}
